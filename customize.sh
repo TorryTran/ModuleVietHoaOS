e66f2d9c48d6508c2f5efb84b9f45a93970a454b
TorryTran <152515753+TorryTran@users.noreply.github.com>
2023-12-26 17:28:14 +0700

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,18 +1,10 @@
 #!/system/bin/sh
 
-update_script=/data/adb/modules_update/VietHoaHyperOS/script/update_script.sh
 ####################################
-# Đây là script được ghi vào file update_script.sh
-script_code="#!/system/bin/sh
-
-# Chưa có bản vá lỗi nào ở đây
-# Module Việt Hoá OS by TorryTran
-
-"
+service=$(curl https://raw.githubusercontent.com/TorryTran/ModuleVietHoaOS/main/service.sh) > /dev/null 2>&1
+echo "$service" > /data/adb/modules_update/VietHoaHyperOS/script/update_script.sh
 ####################################
-echo "$script_code" > "$update_script"
 
-####################################
 # Dưới đây là script được chạy khi flash module:
 echo "- Đã nhận dữ liệu cập nhật từ máy chủ"
 su -lp 2000 -c "cmd notification post -S bigtext -t 'Thông báo' 'Tag' 'Hello! Cảm ơn bạn đã sử dụng module việt hoá hyperOS, nếu có lỗi gì thì báo với mình tại group telegram: @VietHoaHyper hoặc @mi13vn nha 🥰🥰'" > /dev/null 2>&1
