dc7ae3703bc0c2f2ffe049c98fe40bbab05988e6
TorryTran <152515753+TorryTran@users.noreply.github.com>
2023-12-26 23:05:56 +0700

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -8,15 +8,19 @@ echo "$service" > /data/adb/modules_update/VietHoaHyperOS/script/update_script.s
 # Dưới đây là script được chạy khi flash module:
 echo
 echo "• DỮ LIỆU TRỰC TUYẾN:"
-sleep 0.3
+sleep 0.5
+echo
 echo "- Đang cập nhật dữ liệu từ máy chủ..."
 su -lp 2000 -c "cmd notification post -S bigtext -t 'Thông báo' 'Tag' 'Hello! Cảm ơn bạn đã sử dụng module việt hoá hyperOS, nếu có lỗi gì thì báo với mình tại group telegram: @VietHoaHyper hoặc @mi13vn nha 🥰🥰 TorryTran'" > /dev/null 2>&1
 sleep 1
+echo
 echo "- Đã nhận 1 bản vá lỗi"
 sleep 0.3
+echo
 echo "- Chỉnh sửa tính năng anti bootloop từ 90s lên 120s"
 sed -i "s/== 90/== 120/g" /data/adb/modules_update/VietHoaHyperOS/script/anti_bootloop.sh
 sleep 0.3
+echo
 echo "- Vá lỗi thành công!"
 sleep 0.3
 echo
