a9d87012f63e87ccd28bcbe68f3fee87fec47e46
TorryTran <152515753+TorryTran@users.noreply.github.com>
2023-12-26 18:22:05 +0700

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,4 +1,3 @@
 #!/system/bin/sh
 
-# Sắp ra mắt bản vá lỗi trực tuyến, chỉ cần flash module cũ thì lỗi cũng được vá! 
-# update_script.sh là một file được nhận code trực tuyến về thực thi lệnh! nên trong module thì file này đang trong trạng thái rỗng
+# Chưa có bản vá lỗi
