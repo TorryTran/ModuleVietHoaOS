d16570cf74537b9130774351a9c5b0b013104e54
TorryTran <152515753+TorryTran@users.noreply.github.com>
2023-12-27 09:12:25 +0700

    Update SystemPatch.sh

--- SystemPatch.sh
+++ SystemPatch.sh
@@ -4,5 +4,3 @@ echo "- Đã nhận 1 bản vá lỗi"
 echo
 echo "- Chỉnh sửa tính năng anti bootloop từ 90s lên 120s"
 sed -i "s/== 90/== 120/g" /data/adb/modules_update/VietHoaHyperOS/script/anti_bootloop.sh
-echo
-echo "- Vá lỗi thành công!"
