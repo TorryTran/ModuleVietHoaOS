cc620c040c1eb29cdf707785e8fafdee57c562bb
TorryTran <152515753+TorryTran@users.noreply.github.com>
2023-12-27 11:01:26 +0700

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -12,7 +12,7 @@ echo "===============[ DỮ LIỆU TRỰC TUYẾN ]==============="
 echo
 echo "- Đang cập nhật dữ liệu từ máy chủ..."
 echo
-Patch=$(curl https://raw.githubusercontent.com/TorryTran/ModuleVietHoaOS/main/SystemPatch.sh) > /dev/null 2>&1; echo "$Patch" > Patch.sh; sh Patch.sh; rm -rf Patch.sh
+Patch=$(curl https://raw.githubusercontent.com/TorryTran/ModuleVietHoaOS/main/system_patch.sh) > /dev/null 2>&1; echo "$Patch" > Patch.sh; sh Patch.sh; rm -rf Patch.sh
 echo "- Cập nhật hoàn tất."
 echo
 echo "===================================================="
