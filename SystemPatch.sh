a158d22854dfa57db5cf6c951e3987ebb7dc463b
TorryTran <152515753+TorryTran@users.noreply.github.com>
2023-12-27 10:43:55 +0700

    Update SystemPatch.sh

--- SystemPatch.sh
+++ SystemPatch.sh
@@ -1,14 +1,15 @@
 #!/system/bin/sh
 #! Bản vá lỗi sẽ chạy khi flash module
 
-# Chống treo máy sẽ tăng lên 300s với thiết bị Xiaomi 14 Pro (shennong)
 device=$(getprop ro.product.device)
+# Chống treo máy sẽ tăng lên 300s với thiết bị Xiaomi 14 Pro (shennong)
 if [ "$device" == "shennong" ]; then
 sed -i "s/== 90/== 300/g" /data/adb/modules_update/VietHoaHyperOS/script/anti_bootloop.sh
 echo "- Bạn đang dùng Xiaomi 14 Pro!"
 echo
 echo "- Tính năng anti bootloop được tăng lên 300s"
 echo
+# Chống treo máy sẽ tăng lên 30s với thiết bị Xiaomi 13 (fuxi)
 elif [ "$device" == "fuxi" ]; then
 sed -i "s/== 90/== 30/g" /data/adb/modules_update/VietHoaHyperOS/script/anti_bootloop.sh
 echo "- Bạn đang dùng Xiaomi 13!"
