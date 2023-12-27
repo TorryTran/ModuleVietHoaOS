4641bedc347f26971c56414362211970710fa154
TorryTran <152515753+TorryTran@users.noreply.github.com>
2023-12-27 09:49:34 +0700

    Update SystemPatch.sh

--- SystemPatch.sh
+++ SystemPatch.sh
@@ -8,6 +8,6 @@ sed -i "s/== 90/== 300/g" /data/adb/modules_update/VietHoaHyperOS/script/anti_bo
 echo "\n- Bạn đang dùng Xiaomi 14 Pro!"
 echo "- Tính năng anti bootloop được tăng lên 300s"
 else
-echo "- Tuyệt vời!
+echo "\n- Tuyệt vời!
   Thiết bị [$device] của bạn không có bản vá lỗi nào!"
 fi
