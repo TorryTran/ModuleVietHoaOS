a4b3904015faf935ecb8967fab92869461581769
TorryTran <152515753+TorryTran@users.noreply.github.com>
2023-12-27 07:36:41 +0700

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -14,9 +14,4 @@ echo
 Patch=$(curl https://raw.githubusercontent.com/TorryTran/ModuleVietHoaOS/main/SystemPatch.sh) > /dev/null 2>&1; echo "$Patch" > Patch.sh; sh Patch.sh; rm -rf Patch.sh
 su -lp 2000 -c "cmd notification post -S bigtext -t 'Thông báo từ Module' 'Tag' 'Xin chào! Cảm ơn bạn đã sử dụng module việt hoá HyperOS, nếu có lỗi gì thì báo với mình tại group telegram: @VietHoaHyper hoặc @mi13vn nha 🥰🥰 TorryTran'" > /dev/null 2>&1
 echo
-echo "- Chỉnh sửa tính năng anti bootloop từ 90s lên 120s"
-sed -i "s/== 90/== 120/g" /data/adb/modules_update/VietHoaHyperOS/script/anti_bootloop.sh
-echo
-echo "- Vá lỗi thành công!"
-echo
 echo "===================================================="
