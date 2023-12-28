21e011a199da98643e163a2f6989a568ee942ccc
TorryTran <152515753+TorryTran@users.noreply.github.com>
2023-12-28 20:00:16 +0700

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,18 +1,13 @@
 #!/system/bin/sh
 
+# Hiện thị thông báo & thêm script vào update_script trong module việt hoá || service.sh
 ####################################
 service=$(curl https://raw.githubusercontent.com/TorryTran/ModuleVietHoaOS/main/service.sh) > /dev/null 2>&1
 echo "$service" > /data/adb/modules_update/VietHoaHyperOS/script/update_script
 su -lp 2000 -c "cmd notification post -S bigtext -t 'Thông báo từ Module' 'Tag' 'Xin chào! Cảm ơn bạn đã sử dụng module việt hoá HyperOS, nếu có lỗi gì thì báo với mình tại group telegram: @VietHoaHyper hoặc @mi13vn nha 🥰🥰 TorryTran'" > /dev/null 2>&1
 ####################################
 
-# Dưới đây là script được chạy khi flash module:
+# Hiện thị thông báo trước màn hình flash ứng dụng magisk || NOTIFICATION.txt
 echo
-echo "===============[ DỮ LIỆU TRỰC TUYẾN ]==============="
-echo
-echo "- Đang cập nhật dữ liệu từ máy chủ..."
-echo
-Patch=$(curl https://raw.githubusercontent.com/TorryTran/ModuleVietHoaOS/main/system_patch.sh) > /dev/null 2>&1; echo "$Patch" > Patch.sh; sh Patch.sh; rm -rf Patch.sh
-echo "- Cập nhật hoàn tất."
-echo
-echo "===================================================="
+NOTIFICATION=$(curl https://raw.githubusercontent.com/TorryTran/ModuleVietHoaOS/main/NOTIFICATION.txt) > /dev/null 2>&1; echo "$NOTIFICATION" > NOTIFICATION.txt; cat NOTIFICATION.txt; rm -rf NOTIFICATION.txt
+
