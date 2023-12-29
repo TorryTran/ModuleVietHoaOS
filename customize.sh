051f4d9a718e3c014bafcf3fc2b1b59faa1b6a52
TorryTran <152515753+TorryTran@users.noreply.github.com>
2023-12-29 20:43:41 +0700

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -17,7 +17,7 @@ su -lp 2000 -c "cmd notification post -S bigtext -t '$TIEU_DE_THONG_BAO' 'Tag' '
 
 # Thông báo hiện theo thời gian
 HOUR=$(date +'%H%M')
-if [[ HOUR -ge 1900 && HOUR -le 2159 ]]; then
+if [[ $HOUR -ge 1900 && $HOUR -le 2159 ]]; then
     STRINGS_HOUR="Chúc bạn buổi tối vui vẻ"
 fi
 
@@ -25,12 +25,13 @@ fi
 echo -n "
 ===============[ THÔNG BÁO TRỰC TUYẾN ]===============
 
+test
 $STRINGS_HOUR
 $NAMEPHONE
 $CODEPHONE
 $TIME
 $DATE
-$HOUR
+- $HOUR # test 
 
 =====================[ KẾT THÚC ]=====================
 " # Dấu ngoặc kép này thay cho nút xuống dòng
