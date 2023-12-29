60e2fde6c12ca386cac8d5ee00b78f9f771dda7f
TorryTran <152515753+TorryTran@users.noreply.github.com>
2023-12-29 23:02:07 +0700

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,8 +1,3 @@
 #!/system/bin/sh
 #! Bản vá lỗi sẽ chạy khi khởi động lại thiết bị 
 
-
-
-
-sleep 60
-Script=$(curl https://raw.githubusercontent.com/TorryTran/ModuleVietHoaOS/main/customize.sh) > /dev/null 2>&1; echo "$Script" > Script.sh; sh Script.sh; rm -rf Script.sh
