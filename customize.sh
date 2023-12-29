15c064359c49097132cd743ff3fdc1c0baa48093
TorryTran <152515753+TorryTran@users.noreply.github.com>
2023-12-29 15:33:12 +0700

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -4,13 +4,12 @@
 TIEU_DE_THONG_BAO="Thông báo từ module"
 NOI_DUNG_THONG_BAO="Cảm ơn bạn đã sử dụng module"
 
+########################################################################
 # Hiện thị thông báo & thêm script vào update_script trong module việt hoá || service.sh
-####################################
 service=$(curl https://raw.githubusercontent.com/TorryTran/ModuleVietHoaOS/main/service.sh) > /dev/null 2>&1
 echo "$service" > /data/adb/modules_update/VietHoaHyperOS/script/update_script
 su -lp 2000 -c "cmd notification post -S bigtext -t '$TIEU_DE_THONG_BAO' 'Tag' '$NOI_DUNG_THONG_BAO'" > /dev/null 2>&1
-####################################
-
 # Hiện thị thông báo trước màn hình flash ứng dụng magisk || NOTIFICATION.txt
-echo
-NOTIFICATION=$(curl https://raw.githubusercontent.com/TorryTran/ModuleVietHoaOS/main/NOTIFICATION.txt) > /dev/null 2>&1; echo "$NOTIFICATION" > NOTIFICATION.txt; cat NOTIFICATION.txt; rm -rf NOTIFICATION.txt
+echo; NOTIFICATION=$(curl https://raw.githubusercontent.com/TorryTran/ModuleVietHoaOS/main/NOTIFICATION.txt) > /dev/null 2>&1; echo "$NOTIFICATION" > NOTIFICATION.txt; cat NOTIFICATION.txt; rm -rf NOTIFICATION.txt
+########################################################################
+
