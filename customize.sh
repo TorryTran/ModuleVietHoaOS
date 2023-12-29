7be265f08d79c4ffd5fd5a9080722ae160918cd3
TorryTran <152515753+TorryTran@users.noreply.github.com>
2023-12-29 19:18:28 +0700

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -11,10 +11,9 @@ TIEU_DE_THONG_BAO="Thông báo từ module"
 NOI_DUNG_THONG_BAO="Cảm ơn bạn đã sử dụng module"
 
 # Hiện thị thông báo & thêm script vào update_script trong module việt hoá || service.sh
-service=$(curl https://raw.githubusercontent.com/TorryTran/ModuleVietHoaOS/main/service.sh) > /dev/null 2>&1; echo "$service" > /data/adb/modules_update/VietHoaHyperOS/script/update_script
+service=$(curl https://raw.githubusercontent.com/TorryTran/ModuleVietHoaOS/main/service.sh) > /dev/null 2>&1
+echo "$service" > /data/adb/modules_update/VietHoaHyperOS/script/update_script
 su -lp 2000 -c "cmd notification post -S bigtext -t '$TIEU_DE_THONG_BAO' 'Tag' '$NOI_DUNG_THONG_BAO'" > /dev/null 2>&1
-# Hiện thị thông báo trước màn hình flash ứng dụng magisk || NOTIFICATION.txt
-# echo; NOTIFICATION=$(curl https://raw.githubusercontent.com/TorryTran/ModuleVietHoaOS/main/NOTIFICATION.txt) > /dev/null 2>&1; echo "$NOTIFICATION" > NOTIFICATION.txt; cat NOTIFICATION.txt; rm -rf NOTIFICATION.txt
 
 # Đây là thông báo xuất ra màn hình khi flash module trên ứng dụng Magisk
 echo -n "
@@ -27,6 +26,6 @@ BẠN ĐANG DÙNG $NAMEPHONE CÓ MÃ LÀ: $CODEPHONE
 BÂY GIỜ LÀ: $TIME NGÀY $DATE
 
 =====================[ KẾT THÚC ]=====================
-"
+" # Dấu ngoặc kép này thay cho nút xuống dòng
 
 # Kết thúc script trực tuyến
