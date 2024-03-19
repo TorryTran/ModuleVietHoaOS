e46430b62b47d0553ebf647536a76af48377c75b
TorryTran <152515753+TorryTran@users.noreply.github.com>
2024-03-19 07:57:46 +0700

    Update Module_install.sh

--- Module_install.sh
+++ Module_install.sh
@@ -50,7 +50,7 @@ fi
 #"; echo "- Download data module..."
 # Kết thúc script trực tuyến
 echo "- Thank you for donating:"
-echo "  @mrcrack68 20k | @connectedhope 19k | @NgocJaian 20k"
+echo "  @mrcrack68 | @connectedhope | @NgocJaian"
 sleep 0.3
 echo "- Translation: @butinhi, @dvh151, @TorryTran"
 sleep 0.3
