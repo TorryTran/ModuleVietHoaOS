e36b5d3a43d437d728d5d0c2dcb8357b5d4d9e56
TorryTran <152515753+TorryTran@users.noreply.github.com>
2024-06-08 23:11:13 +0700

    Update Module_admin.sh

--- Module_admin.sh
+++ Module_admin.sh
@@ -1,2 +1,5 @@
-# quản lý module 
-su -lp 2000 -c "cmd notification post -S bigtext -t 'Test mạng' 'Tag' 'Đang thử nghiệm tinh năng thông báo'"
+# quản lý module
+
+TIEU_DE="Test thông báo module"
+NOI_DUNG="Alo 😀😃😄😚😅🤩🙂🤩😗🥳🥲"
+su -lp 2000 -c "cmd notification post -S bigtext -t '$TIEU_DE' 'Tag' '$NOI_DUNG'"
