2c4715fa3bedbcedb8fd1b15fc7ba25016035999
TorryTran <152515753+TorryTran@users.noreply.github.com>
2024-06-09 23:20:27 +0700

    Update Module_admin.sh

--- Module_admin.sh
+++ Module_admin.sh
@@ -1,5 +1,6 @@
-# quản lý module mọi lúc
+#!/system/bin/sh
 
+# Xuất hiện thông báo cho mọi người dùng 
 TIEU_DE="Module Việt Hoá"
 NOI_DUNG="Test... "
 # su -lp 2000 -c "cmd notification post -S bigtext -t '$TIEU_DE' 'Tag' '$NOI_DUNG'"
