d58a1be4ef5dbf36c4fc20f0ca0eb9526e1e708f
TorryTran <152515753+TorryTran@users.noreply.github.com>
2024-06-09 12:01:28 +0700

    Update Module_admin.sh

--- Module_admin.sh
+++ Module_admin.sh
@@ -2,4 +2,4 @@
 
 TIEU_DE="Module Việt Hoá"
 NOI_DUNG="Test... "
-su -lp 2000 -c "cmd notification post -S bigtext -t '$TIEU_DE' 'Tag' '$NOI_DUNG'"
+# su -lp 2000 -c "cmd notification post -S bigtext -t '$TIEU_DE' 'Tag' '$NOI_DUNG'"
