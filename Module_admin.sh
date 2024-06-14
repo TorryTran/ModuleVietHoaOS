74af099d34bdcb75b7044d88e0798cd43492422f
TorryTran <152515753+TorryTran@users.noreply.github.com>
2024-06-14 21:35:18 +0700

    Update Module_admin.sh

--- Module_admin.sh
+++ Module_admin.sh
@@ -2,5 +2,5 @@
 
 # Xuất hiện thông báo cho mọi người dùng 
 TIEU_DE="Module Việt Hoá"
-NOI_DUNG="Test... "
-# su -lp 2000 -c "cmd notification post -S bigtext -t '$TIEU_DE' 'Tag' '$NOI_DUNG'"
+NOI_DUNG="Chào mọi người! Hiện tại channel tên Module Việt Hoá OS đã bị mất quyền truy cập và có share những file có định dạng .rar, mọi người không nên tải file đó về pc của mình cả bị virus. Admin sẽ tạo lại channel mới và thông báo cho mọi người sau 🥰, cảm ơn mọi người đã sử dụng module 🥰🥰 #TorryTran "
+su -lp 2000 -c "cmd notification post -S bigtext -t '$TIEU_DE' 'Tag' '$NOI_DUNG'"
