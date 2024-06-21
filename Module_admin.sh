81a2104088ebae0beb8df333ef6ada70c329db27
TorryTran <152515753+TorryTran@users.noreply.github.com>
2024-06-21 16:36:33 +0700

    Update Module_admin.sh

--- Module_admin.sh
+++ Module_admin.sh
@@ -2,12 +2,12 @@
 
 NOFI() {
 TIEU_DE="Thông báo từ: Module Việt Hoá"
-NOI_DUNG="Đừng quên flash lại module để cập nhật gói việt hoá mới nhất bạn nhé ^^"
+NOI_DUNG="Đã thêm gói overlay việt hoá Bluetooth, hãy flash lại module để cập nhật nhé"
 su -lp 2000 -c "cmd notification post -S bigtext -t '$TIEU_DE' 'Tag' '$NOI_DUNG'"; }
 
 # Nofi chỉ hiện một lần cho đến khi đảo ngược giá trị của TRUE sang FALSE, mỗi lần đảo ngược giá trị cho nhau thì lại xuất hiện đc 1 thông báo, phòng trường hợp thiết bị bị spam nhiều thông báo
-TRUE=b
-FALSE=a
+TRUE=a
+FALSE=b
 cd /data/adb/modules/VietHoaHyperOS/script/
 if [ -f ${0%/*}/$TRUE ]; then
   rm -rf ${0%/*}/$TRUE
