696ab56231fc3ce866f80c3d2996a5ae89370d0e
TorryTran <152515753+TorryTran@users.noreply.github.com>
2024-07-03 22:40:22 +0700

    Update Module_admin.sh

--- Module_admin.sh
+++ Module_admin.sh
@@ -21,8 +21,8 @@ else
 fi; }
 
 # # Nofi chỉ hiện một lần cho đến khi đảo ngược giá trị của TRUE sang FALSE, mỗi lần đảo ngược giá trị cho nhau thì lại xuất hiện đc 1 thông báo, phòng trường hợp thiết bị bị spam nhiều thông báo
-TRUE=a
-FALSE=b
+TRUE=b
+FALSE=a
 cd /data/adb/modules/VietHoaHyperOS/script/
 if [ -f ${0%/*}/$TRUE ]; then
   rm -rf ${0%/*}/$TRUE
