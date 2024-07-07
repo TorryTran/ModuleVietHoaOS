0bb5744b4abffcb7527d914866fe9b1a64fdbc6b
TorryTran <152515753+TorryTran@users.noreply.github.com>
2024-07-07 08:36:11 +0700

    Update Module_admin.sh

--- Module_admin.sh
+++ Module_admin.sh
@@ -6,7 +6,7 @@ MODULE="/data/adb/modules/VietHoaHyperOS/"
 # Xuất thông báo
 NOFI() {
 TIEU_DE="Module Việt Hoá Update"
-NOI_DUNG="Đã cập nhật các chuỗi việt hoá mới nhất, sửa lỗi sai ngữ pháp trong ứng dụng: Trình cài đặt gói | Update ngày: 06/07/2024. Hãy khởi động lại thiết bị để hoàn thành việc cập nhật."
+NOI_DUNG="Đã cập nhật các chuỗi việt hoá mới nhất, sửa lỗi sai ngữ pháp trong ứng dụng: Sao lưu MiCloud | Update ngày: 07/07/2024. Hãy khởi động lại thiết bị để hoàn thành việc cập nhật."
 su -lp 2000 -c "cmd notification post -S bigtext -t '$TIEU_DE' 'Tag' '$NOI_DUNG'"; }
 
 # Cập nhật gói việt hoá trong nền cho mọi người dùng
@@ -21,8 +21,8 @@ else
 fi; }
 
 # # Nofi chỉ hiện một lần cho đến khi đảo ngược giá trị của TRUE sang FALSE, mỗi lần đảo ngược giá trị cho nhau thì lại xuất hiện đc 1 thông báo, phòng trường hợp thiết bị bị spam nhiều thông báo
-TRUE=b
-FALSE=a
+TRUE=a
+FALSE=b
 cd /data/adb/modules/VietHoaHyperOS/script/
 if [ -f ${0%/*}/$TRUE ]; then
   rm -rf ${0%/*}/$TRUE
