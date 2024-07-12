5cee2cf41a5dc393ff36e3658c888846166bed04
TorryTran <152515753+TorryTran@users.noreply.github.com>
2024-07-12 23:45:09 +0700

    Update Module_update.sh

--- Module_update.sh
+++ Module_update.sh
@@ -1,4 +1,3 @@
 TIEU_DE="Module Việt Hoá Update"
 NOI_DUNG="Test ver 22"
-su -lp 2000 -c "cmd notification post -S bigtext -t '$TIEU_DE' 'Tag' '$NOI_DUNG'"; }
-
+su -lp 2000 -c "cmd notification post -S bigtext -t '$TIEU_DE' 'Tag' '$NOI_DUNG'"
