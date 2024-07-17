1fd05ed45212aa4f04f1a2196362c80e429322d6
TorryTran <152515753+TorryTran@users.noreply.github.com>
2024-07-17 07:38:44 +0700

    Update Module_endflash.sh

--- Module_endflash.sh
+++ Module_endflash.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
 
 # Mở 1 trang web nào đó khi flash xong module
-# website="https://t.me/VietHoaOS/26"
-# su -c am start -a android.intent.action.VIEW -d "$website" > /dev/null 2>&1
+website="https://t.me/VietHoaOS/29"
+su -c am start -a android.intent.action.VIEW -d "$website" > /dev/null 2>&1
           
