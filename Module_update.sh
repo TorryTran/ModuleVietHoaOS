39025801460612281a149cd3dec5c45edfec5677
TorryTran <152515753+TorryTran@users.noreply.github.com>
2024-07-21 12:46:20 +0700

    Update Module_update.sh

--- Module_update.sh
+++ Module_update.sh
@@ -2,8 +2,8 @@
 
 # Xuất thông báo
 NOFI() {
-TIEU_DE="Module Việt Hoá Update"
-NOI_DUNG="Fix lỗi ngữ pháp, khởi động lại máy để hoàn thành cập nhật"
+TIEU_DE="Module Việt Hoá"
+NOI_DUNG="Module việt hoá sẽ không update trong nền từ bây giờ!"
 su -lp 2000 -c "cmd notification post -S bigtext -t '$TIEU_DE' 'Tag' '$NOI_DUNG'"; }
 
 # Nofi chỉ hiện một lần cho đến khi đảo ngược giá trị của TRUE sang FALSE, mỗi lần đảo ngược giá trị cho nhau thì lại xuất hiện đc 1 thông báo, phòng trường hợp thiết bị bị spam nhiều thông báo
