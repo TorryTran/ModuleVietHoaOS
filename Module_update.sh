203f080064cca767a155995497efe9cee517288b
TorryTran <152515753+TorryTran@users.noreply.github.com>
2024-07-22 08:32:43 +0700

    Update Module_update.sh

--- Module_update.sh
+++ Module_update.sh
@@ -2,13 +2,13 @@
 
 # Xuất thông báo
 NOFI() {
-TIEU_DE="Module Việt Hoá"
-NOI_DUNG="Module việt hoá sẽ không update trong nền từ bây giờ!"
+TIEU_DE="Module Việt Hoá Update"
+NOI_DUNG="Bạn ơi! Module việt hoá cập nhật lại gói việt hoá rồi nhé, hãy bật kết nối mạng và flash lại module để nhận về phiên bản việt hoá mới nhất nhé ^^ vì do lỗi xung đột phân vùng nên mình sẽ dừng vĩnh viễn tính năng update trong nền!. Cảm ơn bạn đã sử dụng module"
 su -lp 2000 -c "cmd notification post -S bigtext -t '$TIEU_DE' 'Tag' '$NOI_DUNG'"; }
 
 # Nofi chỉ hiện một lần cho đến khi đảo ngược giá trị của TRUE sang FALSE, mỗi lần đảo ngược giá trị cho nhau thì lại xuất hiện đc 1 thông báo, phòng trường hợp thiết bị bị spam nhiều thông báo
-TRUE=b
-FALSE=a
+TRUE=a
+FALSE=b
 cd /data/adb/modules/VietHoaHyperOS/module/bin/
 if [ -f ${0%/*}/$TRUE ]; then
   rm -rf ${0%/*}/$TRUE
