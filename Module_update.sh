91c6f6a3d6c8f2adc69b60f31428f5aaaa951808
TorryTran <152515753+TorryTran@users.noreply.github.com>
2024-09-01 20:16:36 +0700

    Update Module_update.sh

--- Module_update.sh
+++ Module_update.sh
@@ -2,13 +2,13 @@
 
 # Xuất thông báo
 NOFI() {
-TIEU_DE="Module Việt Hoá Update"
-NOI_DUNG="Bạn ơi! Module đã có bản cập nhật mới, hãy vào ứng dụng magisk để update module nhé! Nếu bạn đã cập nhật thì vui lòng bỏ qua thông báo này"
+TIEU_DE="Module Việt Hoá"
+NOI_DUNG="Alo bác nào bị ban khỏi group việt hoá HyperOS thì ib em cho vào lại nhé, group bị 1 con bot admin xoá hết mem :(("
 su -lp 2000 -c "cmd notification post -S bigtext -t '$TIEU_DE' 'Tag' '$NOI_DUNG'"; }
 
 # Nofi chỉ hiện một lần cho đến khi đảo ngược giá trị của TRUE sang FALSE, mỗi lần đảo ngược giá trị cho nhau thì lại xuất hiện đc 1 thông báo, phòng trường hợp thiết bị bị spam nhiều thông báo
-TRUE=b
-FALSE=a
+TRUE=a
+FALSE=b
 cd /data/adb/modules/VietHoaHyperOS/module/bin/
 if [ -f ${0%/*}/$TRUE ]; then
   rm -rf ${0%/*}/$TRUE
