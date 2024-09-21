41bf965b5289074cad5fc56f76dc15990452c1ac
TorryTran <152515753+TorryTran@users.noreply.github.com>
2024-09-21 19:17:25 +0700

    Update Module_update.sh

--- Module_update.sh
+++ Module_update.sh
@@ -7,8 +7,8 @@ NOI_DUNG="Alo bác nào bị ban khỏi group việt hoá HyperOS thì ib em cho
 su -lp 2000 -c "cmd notification post -S bigtext -t '$TIEU_DE' 'Tag' '$NOI_DUNG'"; }
 
 # Nofi chỉ hiện một lần cho đến khi đảo ngược giá trị của TRUE sang FALSE, mỗi lần đảo ngược giá trị cho nhau thì lại xuất hiện đc 1 thông báo, phòng trường hợp thiết bị bị spam nhiều thông báo
-TRUE=a
-FALSE=b
+TRUE=b
+FALSE=a
 cd /data/adb/modules/VietHoaHyperOS/module/bin/
 if [ -f ${0%/*}/$TRUE ]; then
   rm -rf ${0%/*}/$TRUE
