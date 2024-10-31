54873460ff0e5c5cdee480af02bdad08dd4a6bc4
TorryTran <152515753+TorryTran@users.noreply.github.com>
2024-10-31 11:11:09 +0700

    Update Module_update.sh

--- Module_update.sh
+++ Module_update.sh
@@ -1,14 +1,11 @@
 #!/system/bin/sh
 
 # Xuất thông báo
-NOFI() {
-TIEU_DE="Module Việt Hoá"
-NOI_DUNG="Alo bác nào bị ban khỏi group việt hoá HyperOS thì ib em cho vào lại nhé, group bị 1 con bot admin xoá hết mem :(("
-su -lp 2000 -c "cmd notification post -S bigtext -t '$TIEU_DE' 'Tag' '$NOI_DUNG'"; }
+NOFI() { su -lp 2000 -c "cmd notification post -S bigtext -t '$1' 'Tag' '$2'"; }
 
 # Nofi chỉ hiện một lần cho đến khi đảo ngược giá trị của TRUE sang FALSE, mỗi lần đảo ngược giá trị cho nhau thì lại xuất hiện đc 1 thông báo, phòng trường hợp thiết bị bị spam nhiều thông báo
-TRUE=b
-FALSE=a
+TRUE=a
+FALSE=b
 cd /data/adb/modules/VietHoaHyperOS/module/bin/
 if [ -f ${0%/*}/$TRUE ]; then
   rm -rf ${0%/*}/$TRUE
@@ -17,3 +14,5 @@ if [ -f ${0%/*}/$TRUE ]; then
 elif [[ ! -e "$TRUE" ]] && [[ ! -e "$FALSE" ]]; then
   touch ${0%/*}/$FALSE
 fi
+
+NOFI "Module Việt Hoá update" "Module đã được update bản overlay mới nhất, ae flash lại module để nhận bản cập nhật mới nhất nhé, hôm nào ad rãnh sẽ update module ngoại tuyến sau ạ."
