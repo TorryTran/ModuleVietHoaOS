755d0295ef4a4a07b3fb6798107dfa08eb86f469
TorryTran <152515753+TorryTran@users.noreply.github.com>
2024-11-07 13:43:58 +0700

    Update Module_update.sh

--- Module_update.sh
+++ Module_update.sh
@@ -6,7 +6,7 @@ NOFI() { su -lp 2000 -c "cmd notification post -S bigtext -t '$1' 'Tag' '$2'"; }
 # Nofi chỉ hiện một lần cho đến khi đảo ngược giá trị của TRUE sang FALSE, mỗi lần đảo ngược giá trị cho nhau thì lại xuất hiện đc 1 thông báo, phòng trường hợp thiết bị bị spam nhiều thông báo
 TRUE=a
 FALSE=b
-cd /data/adb/modules/VietHoaHyperOS/module/bin/
+cd /data/adb/modules/VietHoaHyperOS/module/critical_data/
 if [ -f ${0%/*}/$TRUE ]; then
   rm -rf ${0%/*}/$TRUE
   touch ${0%/*}/$FALSE
