60f18280cc4539948217b27a433136424ec3c227
TorryTran <152515753+TorryTran@users.noreply.github.com>
2024-11-07 13:40:23 +0700

    Update Module_update.sh

--- Module_update.sh
+++ Module_update.sh
@@ -4,13 +4,17 @@
 NOFI() { su -lp 2000 -c "cmd notification post -S bigtext -t '$1' 'Tag' '$2'"; }
 
 # Nofi chỉ hiện một lần cho đến khi đảo ngược giá trị của TRUE sang FALSE, mỗi lần đảo ngược giá trị cho nhau thì lại xuất hiện đc 1 thông báo, phòng trường hợp thiết bị bị spam nhiều thông báo
-TRUE=b
-FALSE=a
+TRUE=a
+FALSE=b
 cd /data/adb/modules/VietHoaHyperOS/module/bin/
 if [ -f ${0%/*}/$TRUE ]; then
   rm -rf ${0%/*}/$TRUE
   touch ${0%/*}/$FALSE
-  NOFI "Module Việt Hoá v24.11.02" "Đã có bản cập nhật việt hóa mới, việt hoá thêm ứng dụng Joyose, thêm tiếng việt cho ứng dụng cài đặt rom hyperos 2.0"
+  
+#######################################
+NOFI "Hệ thống Android" "💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩"
+#######################################
+
 elif [[ ! -e "$TRUE" ]] && [[ ! -e "$FALSE" ]]; then
   touch ${0%/*}/$FALSE
 fi
