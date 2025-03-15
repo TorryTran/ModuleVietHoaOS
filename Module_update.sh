57b36ccf12f61224977048e347945fd4a3da0fde
TorryTran <152515753+TorryTran@users.noreply.github.com>
2025-03-15 20:54:45 +0700

    Update Module_update.sh

--- Module_update.sh
+++ Module_update.sh
@@ -4,8 +4,8 @@
 NOFI() { su -lp 2000 -c "cmd notification post -S bigtext -t '$1' 'Tag' '$2'"; }
 
 # Nofi chỉ hiện một lần cho đến khi đảo ngược giá trị của TRUE sang FALSE, mỗi lần đảo ngược giá trị cho nhau thì lại xuất hiện đc 1 thông báo, phòng trường hợp thiết bị bị spam nhiều thông báo
-TRUE=b
-FALSE=a
+TRUE=a
+FALSE=b
 if [ -f ${0%/*}/$TRUE ]; then
   rm -rf ${0%/*}/$TRUE
   touch ${0%/*}/$FALSE
