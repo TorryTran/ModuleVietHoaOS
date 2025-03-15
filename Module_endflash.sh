1c47391de839ab0a2c41b3ae96ab5eb8dd833498
TorryTran <152515753+TorryTran@users.noreply.github.com>
2025-03-15 20:50:03 +0700

    test

--- Module_endflash.sh
+++ Module_endflash.sh
@@ -2,5 +2,5 @@
 
 # Mở 1 trang web nào đó khi flash xong module
 website="https://t.me/VietHoaOS"
-#su -c am start -a android.intent.action.VIEW -d "$website" > /dev/null 2>&1
+su -c am start -a android.intent.action.VIEW -d "$website" > /dev/null 2>&1
           
--- Module_update.sh
+++ Module_update.sh
@@ -4,14 +4,14 @@
 NOFI() { su -lp 2000 -c "cmd notification post -S bigtext -t '$1' 'Tag' '$2'"; }
 
 # Nofi chỉ hiện một lần cho đến khi đảo ngược giá trị của TRUE sang FALSE, mỗi lần đảo ngược giá trị cho nhau thì lại xuất hiện đc 1 thông báo, phòng trường hợp thiết bị bị spam nhiều thông báo
-TRUE=a
-FALSE=b
+TRUE=b
+FALSE=a
 if [ -f ${0%/*}/$TRUE ]; then
   rm -rf ${0%/*}/$TRUE
   touch ${0%/*}/$FALSE
   
 #######################################
-NOFI "Thông báo" "test"
+NOFI "Thông báo" "test2222222222"
 #######################################
 
 elif [[ ! -e "$TRUE" ]] && [[ ! -e "$FALSE" ]]; then
