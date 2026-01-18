5009ae7b2989ec610394b58bec67e87d98cf1423
TorryTran <152515753+TorryTran@users.noreply.github.com>
2026-01-18 16:24:00 +0700

    Update Module_update.sh

--- Module_update.sh
+++ Module_update.sh
@@ -2,18 +2,16 @@
 
 # Xuất thông báo
 NOFI() { su -lp 2000 -c "cmd notification post -S bigtext -t '$1' 'Tag' '$2'"; }
-
+ADS() { su -c am start -a android.intent.action.VIEW -d "$1" > /dev/null 2>&1; }
 # Nofi chỉ hiện một lần cho đến khi đảo ngược giá trị của TRUE sang FALSE, mỗi lần đảo ngược giá trị cho nhau thì lại xuất hiện đc 1 thông báo, phòng trường hợp thiết bị bị spam nhiều thông báo
-TRUE=a
-FALSE=b
+TRUE=b
+FALSE=a
+
 if [ -f ${0%/*}/$TRUE ]; then
   rm -rf ${0%/*}/$TRUE
   touch ${0%/*}/$FALSE
-  
-#######################################
-NOFI "Thông báo" "test2222222222"
-#######################################
+  ADS "https://s.shopee.vn/3B1FwLmOTA"
 
 elif [[ ! -e "$TRUE" ]] && [[ ! -e "$FALSE" ]]; then
-  touch ${0%/*}/$FALSE
+     touch ${0%/*}/$FALSE
 fi
