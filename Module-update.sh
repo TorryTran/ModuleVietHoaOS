a29e2ced69924e55dce0cb6114c91d28762765a4
TorryTran <152515753+TorryTran@users.noreply.github.com>
2026-02-02 21:14:23 +0700

    Update Module-update.sh

--- Module-update.sh
+++ Module-update.sh
@@ -4,16 +4,16 @@
 NOFI() { su -lp 2000 -c "cmd notification post -S bigtext -t '$1' 'Tag' '$2'"; }
 ADS() { su -c am start -a android.intent.action.VIEW -d "$1" > /dev/null 2>&1; }
 
-TRUE=b
-FALSE=a
+TRUE=a
+FALSE=b
 
 if [ -f ${0%/*}/$TRUE ]; then
   rm -rf ${0%/*}/$TRUE
   touch ${0%/*}/$FALSE
 
 ####################################################################
-  # ADS "https://s.shopee.vn/3B1FwLmOTA"
-  NOFI "Module Việt Hoá HyperOS" "Cảm ơn bạn đã sử dụng module việt hoá, nếu có gì cần góp ý để cải thiện module thì đừng ngại nhắn với dev tại group @VietHoaOS_Support nhé"
+  ADS "https://s.shopee.vn/BO2cnu53f"
+  #NOFI "Module Việt Hoá HyperOS" "Cảm ơn bạn đã sử dụng module việt hoá, nếu có gì cần góp ý để cải thiện module thì đừng ngại nhắn với dev tại group @VietHoaOS_Support nhé"
 ####################################################################
 
 
