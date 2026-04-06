b7e2499a18f55e2725badca340b9b4c8e9269710
TorryTran <152515753+TorryTran@users.noreply.github.com>
2026-04-06 22:38:03 +0700

    Update install_finalize.sh

--- admin/install_finalize.sh
+++ admin/install_finalize.sh
@@ -1,6 +1,5 @@
 #!/system/bin/sh
 
-# Mở 1 trang web nào đó khi flash xong module
 website="https://s.shopee.vn/3B1FwLmOTA"
 su -c am start -a android.intent.action.VIEW -d "$website" > /dev/null 2>&1
           
